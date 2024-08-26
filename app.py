6f8c95bd129c6e5595f9b31ba5b02c6fea5f791b
Narendra@523116 <nalamothunarendra@gmail.com>
2024-08-26 10:09:15 +0530

    made chnages to the html and app.js for not to reload for every input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import pickle
 import numpy as np
 
@@ -17,20 +17,19 @@ def index():
     prediction = None
     if request.method == 'POST':
         user_input = request.form['user_input']
+        print(user_input)
         
         # Transform the input using the loaded vectorizer
         user_input_transformed = vectorizer.transform([user_input])
         
-        # Reshape if necessary
-        # user_input_transformed = user_input_transformed.toarray().reshape(1, -1)
         
-        # Make a prediction using the transformed input
         prediction = model.predict(user_input_transformed)[0]
+        print(prediction)
         encoded = {0: 'Business', 1: 'Crime', 2: 'Education', 3: 'Entertainment', 4: 'Environment', 5: 'Health', 6: 'Nation',7: 'Politics', 8: 'Religion', 9: 'Science', 10: 'Sports', 11: 'Technology', 12: 'Travel', 13: 'World'}
         
-        return render_template('index.html', prediction=encoded[prediction])
+        return jsonify(prediction=encoded[prediction])
     
-    return render_template('index.html', prediction=prediction)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
